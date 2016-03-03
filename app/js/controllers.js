0faa7b5d553099a7fb41a8073aec0b3957784310
iopqrst <opqrst@163.com>
2016-03-03 22:48:12 +0800

    no message

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,14 +5,21 @@ var clientControllers = angular.module('ClientControllers', []);
 
 clientControllers.controller('ClientListController', ['$scope', 'Client',
 	function($scope, Client) {
-		Client.queryList({
-
-		}, function(data) {
-			if( data && data.code == 1) {
-				$scope.clientList = data.data.clientList;
-			}
-		}, function(error) {
-
-		});
+		$scope.client = {};
+		
+		this.queryList = function(){
+			Client.list($scope.client).success(function(data) {
+				if (data && data.code == 1) {
+					$scope.clientList = data.data.clientList;
+				}
+			});
+		}
+		
+		this.queryList(); //init
+		var that = this;
+		
+		$scope.search = function(){
+			that.queryList();
+		}
 	}
 ]);
--- app/js/services.js
+++ app/js/services.js
@@ -1,20 +1,40 @@
 'use strict';
 
 /* Services */
-var clientServices = angular.module('MyClientServices', ['ngResource']);
+var clientServices = angular.module('MyClientServices', []);
 
-clientServices.factory('Client', ['$resource', function($resource) {
-	return $resource('data/:data', {}, {
-		queryList: {
-			method: 'GET',
-			params: {
-				data: 'client-list.json'
-			}
+//clientServices.factory('Client', ['$resource', function($resource) {
+//	return $resource('data/:data', {}, {
+//		queryList: {
+//			method: 'GET',
+//			params: {
+//				data: 'client-list.json'
+//			}
+//		},
+//		add: {
+//
+//		}
+//	})
+//}]);
+
+clientServices.factory('Client', ['$http', function($http) {
+	return {
+		list: function(params) {
+			console.info (params );
+			return $http({
+				method: 'post',
+				url: '/client/list',
+				params : {
+					mobile : params.mobile,
+					name : params.name
+				}
+			})
 		},
-		add: {
+		add: function(params) {
 
 		}
-	})
+	};
 }]);
+
 
 // $resource(url [, paramDefaults 可选 对象] , action[可选，对象]);
